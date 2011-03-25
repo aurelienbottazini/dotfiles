6798fcae0b2a3984617008674ef6fd04c29141b5
Aurélien Bottazzini <aurelien.bottazzini@gmail.com>
2011-03-25 10:20:03 +0100

    removing useless ln

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 ln -sf ~/dotfiles/zshrc ~/.zshrc
 ln -sf ~/dotfiles/gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/zshenv ~/.zshenv
-ln -sf ~/dotfiles/zsh ~/.zsh
 ln -sf ~/dotfiles/screenrc ~/.screenrc
 ln -sf ~/dotfiles/rspec ~/.rspec
 ln -sf ~/dotfiles/autotest ~/.autotest

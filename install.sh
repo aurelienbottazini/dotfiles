417c0466588d5163d6d308e81f59bd1ae324a55a
Aurélien Bottazzini <aurelien.bottazzini@gmail.com>
2011-03-31 15:49:03 +0200

    adding bashrc

--- install.sh
+++ install.sh
@@ -4,3 +4,5 @@ ln -sf ~/dotfiles/zshenv ~/.zshenv
 ln -sf ~/dotfiles/screenrc ~/.screenrc
 ln -sf ~/dotfiles/rspec ~/.rspec
 ln -sf ~/dotfiles/autotest ~/.autotest
+ln -sf ~/dotfiles/bashrc ~/.bashrc
+ln -sf ~/dotfiles/profile ~/.profile

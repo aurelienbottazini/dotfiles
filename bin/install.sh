d3ae4ce76f9a6b7a6e5c9d8ad99c87a65c0c80fc
Aurélien Bottazzini <contact@aurelienbottazini.com>
2015-05-28 19:44:09 +0200

    small install bugs

--- bin/install.sh
+++ bin/install.sh
@@ -1,20 +1,22 @@
 #!/usr/bin/env zsh
 
 brew tap Goles/battery
-brew install reattach-to-user-namespace battery fzf
+brew install reattach-to-user-namespace battery fzf coreutils git
+
+git clone --recursive git@github.com:aurelienbottazini/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+
 setopt EXTENDED_GLOB
 for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
   ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
 
+ln -sf ~/projects/dotfiles/Vagrantfile ~/Vagrantfile
 ln -sf ~/projects/dotfiles/gitconfig ~/.gitconfig
 ln -sf ~/projects/dotfiles/gitignore_global ~/.gitignore_global
 ln -sf ~/projects/dotfiles/screenrc ~/.screenrc
 ln -sf ~/projects/dotfiles/rspec ~/.rspec
 ln -sf ~/projects/dotfiles/autotest ~/.autotest
 ln -sf ~/projects/dotfiles/tmux.conf ~/.tmux.conf
-ln -sf ~/projects/dotfiles/zshrc ~/.zshrc
-ln -sf ~/projects/dotfiles/aure.zsh-theme ~/.oh-my-zsh/themes/aure.zsh-theme
 ln -sf ~/projects/dotfiles/dictionaries/en_US.aff ~/Library/Spelling/en_US_aurelien.aff
 ln -sf ~/projects/dotfiles/dictionaries/en_US.dic ~/Library/Spelling/en_US_aurelien.dic
 ln -sf ~/projects/dotfiles/dictionaries/fr_FR.aff ~/Library/Spelling/fr_FR.aff

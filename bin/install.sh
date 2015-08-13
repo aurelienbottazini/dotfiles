4a7471ff13a5cdf1467fe7c733e300bf25a2a949
Aurélien Bottazzini <contact@aurelienbottazini.com>
2015-08-13 08:32:42 +0200

    changing ln path for install script

--- bin/install.sh
+++ bin/install.sh
@@ -11,28 +11,28 @@ npm install -g grunt-cli
 
 gem install rubocop sass ruby-beautify htmlbeautifier
 
-git clone --recursive git@github.com:aurelienbottazini/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+# git clone --recursive git@github.com:aurelienbottazini/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 
-setopt EXTENDED_GLOB
-for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
-  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
-done
+# setopt EXTENDED_GLOB
+# for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+  # ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+# done
 
-ln -sf ~/projects/dotfiles/Vagrantfile ~/Vagrantfile
-ln -sf ~/projects/dotfiles/gitconfig ~/.gitconfig
-ln -sf ~/projects/dotfiles/gitignore_global ~/.gitignore_global
-ln -sf ~/projects/dotfiles/screenrc ~/.screenrc
-ln -sf ~/projects/dotfiles/rspec ~/.rspec
-ln -sf ~/projects/dotfiles/autotest ~/.autotest
-ln -sf ~/projects/dotfiles/tmux.conf ~/.tmux.conf
-ln -sf ~/projects/dotfiles/dictionaries/en_US.aff ~/Library/Spelling/en_US_aurelien.aff
-ln -sf ~/projects/dotfiles/dictionaries/en_US.dic ~/Library/Spelling/en_US_aurelien.dic
-ln -sf ~/projects/dotfiles/dictionaries/fr_FR.aff ~/Library/Spelling/fr_FR.aff
-ln -sf ~/projects/dotfiles/dictionaries/fr_FR.dic ~/Library/Spelling/fr_FR.dic
-ln -sf ~/projects/dotfiles/dictionaries/fr_FR.aff ~/Library/Spelling/fr_FR_aurelien.aff
-ln -sf ~/projects/dotfiles/dictionaries/fr_FR.dic ~/Library/Spelling/fr_FR_aurelien.dic
-ln -sf ~/projects/dotfiles/dictionaries/.hunspell_en_US_aurelien ~/.hunspell_en_US_aurelien
-ln -sf ~/projects/dotfiles/dictionaries/.hunspell_fr_FR_aurelien ~/.hunspell_fr_FR_aurelien
-ln -sf ~/projects/dotfiles/tmuxinator ~/.tmuxinator
-ln -sf ~/projects/dotfiles/agignore ~/.agignore
-ln -sf ~/projects/dotfiles/vimrc ~/.vimrc
+ln -sf ~/dotfiles/Vagrantfile ~/Vagrantfile
+ln -sf ~/dotfiles/gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/gitignore_global ~/.gitignore_global
+ln -sf ~/dotfiles/screenrc ~/.screenrc
+ln -sf ~/dotfiles/rspec ~/.rspec
+ln -sf ~/dotfiles/autotest ~/.autotest
+ln -sf ~/dotfiles/tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/dictionaries/en_US.aff ~/Library/Spelling/en_US_aurelien.aff
+ln -sf ~/dotfiles/dictionaries/en_US.dic ~/Library/Spelling/en_US_aurelien.dic
+ln -sf ~/dotfiles/dictionaries/fr_FR.aff ~/Library/Spelling/fr_FR.aff
+ln -sf ~/dotfiles/dictionaries/fr_FR.dic ~/Library/Spelling/fr_FR.dic
+ln -sf ~/dotfiles/dictionaries/fr_FR.aff ~/Library/Spelling/fr_FR_aurelien.aff
+ln -sf ~/dotfiles/dictionaries/fr_FR.dic ~/Library/Spelling/fr_FR_aurelien.dic
+ln -sf ~/dotfiles/dictionaries/.hunspell_en_US_aurelien ~/.hunspell_en_US_aurelien
+ln -sf ~/dotfiles/dictionaries/.hunspell_fr_FR_aurelien ~/.hunspell_fr_FR_aurelien
+ln -sf ~/dotfiles/tmuxinator ~/.tmuxinator
+ln -sf ~/dotfiles/agignore ~/.agignore
+ln -sf ~/dotfiles/vimrc ~/.vimrc

392fa3230353bd2c5d6b69efa037941c15866505
Aurélien Bottazzini <contact@aurelienbottazini.com>
2016-01-04 15:22:16 +0100

    adapting tmux for fish and importing aliases

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -5,3 +5,16 @@ set -x DOCKER_TLS_VERIFY 1
 set -x VAGRANT_DEFAULT_PROVIDER parallels
 
 status --is-interactive; and . (rbenv init -|psub)
+
+alias ec "emacsclient -ta ''"
+alias dcr 'docker-compose run'
+alias dcrs 'docker-compose run --service-ports'
+alias delete_docker_containers 'docker rm (docker ps -a -q)'
+alias delete_docker_images 'docker rmi (docker images -q)'
+
+alias tu "tmux -u"
+alias tmux "tmux -u"
+alias screen "screen -U"
+alias pd "psql -p 5432 -h localhost -d "
+
+alias watch_projects "fswatch -0 -r --exclude '\.#' --exclude vendor --exclude .git --exclude Gemfile.lock --exclude GPATH --exclude GRTAGS --exclude GTAGS --exclude tmp /Users/aurelienbottazini/projects | xargs -0 -n 1 -I file rsync_ks file"

63ee5f581932628b62c8ec52cf1ecc2ead9d9388
Aurélien Bottazzini <contact@aurelienbottazini.com>
2016-01-07 10:52:22 +0100

    cdpath and helm

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,7 +1,8 @@
 set PATH ~/dotfiles/bin/ $PATH
+set -x CDPATH $HOME $HOME/projects $HOME/projects/work
+
 set -x DOCKER_HOST tcp://ks212069.kimsufi.com:2376
 set -x DOCKER_TLS_VERIFY 1
-
 set -x VAGRANT_DEFAULT_PROVIDER parallels
 
 status --is-interactive; and . (rbenv init -|psub)

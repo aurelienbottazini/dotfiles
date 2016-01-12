2c8b66b8efa1552e2af015394a936433d43ab3e2
Aurélien Bottazzini <contact@aurelienbottazini.com>
2016-01-12 16:53:33 +0100

    Org-agenda files

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -8,7 +8,7 @@
  '(company-idle-delay 0.2)
  '(custom-safe-themes
    (quote
-    ("c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "86faa1ca1ea66c2f144a77643ae0bfcf0db08bbee2aa63bafd79053d98dc491c" "8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" default)))
+    ("c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "edd4a89da0e38029db5c58cd6313cfbab222be43fbcddcdacfbf18a86285a216" "8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" default)))
  '(electric-pair-mode nil)
  '(elscreen-tab-display-control nil)
  '(emojify-display-style (quote unicode))
@@ -20,6 +20,7 @@
  '(global-emojify-mode t)
  '(linum-relative-current-symbol "")
  '(magit-push-always-verify nil)
+ '(org-agenda-files (quote ("~/Dropbox/org/gtd.org")))
  '(org-bullets-bullet-list (quote ("◉" "○" "■" "□")))
  '(org-hide-leading-stars t)
  '(ruby-deep-arglist nil)

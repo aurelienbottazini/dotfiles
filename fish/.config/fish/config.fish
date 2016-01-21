7fec1617a790c6ed42e6093cd43c4c2a7bbc6ee6
Aurélien Bottazzini <contact@aurelienbottazini.com>
2016-01-21 18:44:55 +0100

    autocompletion color

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -22,3 +22,4 @@ alias watch_projects "fswatch -0 -r --exclude '\.#' --exclude vendor --exclude .
 
 set --erase fish_greeting
 set -Ux EDITOR 'emacsclient -t -a ""'
+set -x fish_color_autosuggestion yellow

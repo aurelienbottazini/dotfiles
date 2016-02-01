ae545658679e2e2e479031e36b37113af7079b7b
Aurélien Bottazzini <aurelien@bottazini.com>
2016-02-01 11:47:18 +0100

    saving emacs config

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -8,7 +8,7 @@
  '(company-idle-delay 0.2)
  '(custom-safe-themes
    (quote
-    ("a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" default)))
+    ("b04425cc726711a6c91e8ebc20cf5a3927160681941e06bc7900a5a5bfe1a77f" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" default)))
  '(electric-pair-mode nil)
  '(elscreen-tab-display-control nil)
  '(emojify-display-style (quote unicode))
@@ -25,8 +25,8 @@
  '(org-hide-leading-stars t)
  '(ruby-deep-arglist nil)
  '(show-paren-mode nil)
- '(sml/pre-modes-separator (propertize " " (quote face) (quote sml/modes)))
- '(sml/theme (quote respectful)))
+ '(sml/theme (quote respectful))
+ '(visual-line-fringe-indicators (quote (left-curly-arrow right-curly-arrow))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.

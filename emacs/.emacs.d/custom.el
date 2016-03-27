1d13f526a86f1ca12cbc832444983d94065c3526
Aurélien Bottazzini <aurelien@bottazini.com>
2016-03-27 12:41:24 +0200

    Switching to zenburn everywhere

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -12,10 +12,12 @@
    (quote
     ("8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" default)))
  '(deft-directory "/Users/aurelienbottazini/Dropbox/org")
+ '(deft-recursive t)
+ '(deft-use-filename-as-title t)
  '(electric-pair-mode nil)
- '(elscreen-tab-display-control nil t)
+ '(elscreen-tab-display-control nil)
  '(emojify-display-style (quote unicode) t)
- '(evil-toggle-key "C-z")
+ '(evil-toggle-key "C-c e")
  '(exec-path-from-shell-arguments (quote ("-l")))
  '(flycheck-indication-mode nil)
  '(flycheck-ruby-executable "/usr/local/var/rbenv/shims/ruby")
@@ -40,6 +42,9 @@
  '(org-bullets-bullet-list (quote ("•" "†" "‡")))
  '(org-hide-leading-stars t)
  '(org-indent-mode-turns-off-org-adapt-indentation nil)
+ '(package-selected-packages
+   (quote
+    (elscreen zenburn-theme zoom-window yatemplate yaml-mode writeroom-mode writegood-mode wrap-region windresize which-key web-mode web-beautify use-package typescript-mode synosaurus smart-mode-line-powerline-theme scss-mode sass-mode ruby-interpolation rspec-mode relative-line-numbers rainbow-mode rainbow-identifiers rainbow-delimiters projectile-rails project-explorer palimpsest org-bullets ob-typescript neotree markdown-mode load-theme-buffer-local less-css-mode langtool key-chord json-mode js3-mode js2-refactor htmlize highlight-indentation helm-swoop helm-projectile helm-mu helm-gtags helm-ag guide-key git-link ggtags flycheck-typescript-tslint fish-mode fill-column-indicator exec-path-from-shell evil-visualstar evil-surround evil-search-highlight-persist evil-org evil-multiedit evil-matchit evil-magit evil-indent-plus evil-commentary enh-ruby-mode emmet-mode dockerfile-mode deft context-coloring company-tern coffee-mode bundler babel-repl avy all-ext ag)))
  '(ruby-deep-arglist nil)
  '(send-mail-function (quote smtpmail-send-it))
  '(show-paren-mode t)
@@ -61,20 +66,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(sml/git ((t (:foreground "#005c4d" :slant italic))))
- '(sml/minor-modes ((t (:inherit sml/global :slant italic))))
- '(sml/position-percentage ((t (:inherit sml/prefix :slant italic :weight normal))))
- '(sml/projectile ((t (:foreground "#23249e"))))
- '(sml/vc-edited ((t (:foreground "#da0039" :slant italic :weight normal))))
- '(term ((t (:inherit default :background "#f1f2f1" :foreground "#212822"))))
- '(term-color-black ((t (:foreground "#616568"))))
- '(term-color-blue ((t (:foreground "#23249e"))))
- '(term-color-cyan ((t (:foreground "#23249e"))))
- '(term-color-green ((t (:foreground "#005c4d"))))
- '(term-color-magenta ((t (:foreground "#600b92"))))
- '(term-color-red ((t (:foreground "#600b92"))))
- '(term-color-white ((t (:foreground "#616568"))))
- '(term-color-yellow ((t (:foreground "#785a20"))))
- '(term-default-bg-color "#211E1E")
- '(term-default-fg-color "#AAAAAA")
+ '(elscreen-tab-background-face ((t (:background "#4f4f4f" :foreground "#dcdcdc"))))
+ '(elscreen-tab-current-screen-face ((t (:background "#6f6f6f" :foreground "#dcdcdc" :weight bold))))
+ '(elscreen-tab-other-screen-face ((t (:background "#4f4f4f" :foreground "#dcdcdc"))))
  '(variable-pitch ((t (:height 1.2)))))
--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -29,9 +29,46 @@ alias watch_projects "fswatch -0 -r --exclude '\.#' --exclude vendor --exclude .
 
 set --erase fish_greeting
 set -Ux EDITOR 'emacsclient -t -a ""'
-set -Ux fish_color_autosuggestion "#e9eae8"
-set -UX fish_color_command blue
+set -Ux fish_color_autosuggestion "#6f6f6f"
+set -Ux fish_color_command "#94bff3"
+set -Ux fish_color_param "#8cd0d3"
+
 set -Ux fish_term24bit 1
+
+
+
+
+# fish_color_comment	eab700
+# fish_color_cwd	green
+# fish_color_cwd_root	red
+# fish_color_end	8959a8
+# fish_color_error	c82829
+# fish_color_escape	cyan
+# fish_color_history_current	cyan
+# fish_color_host	'-o' 'cyan'
+# fish_color_match	cyan
+# fish_color_normal	normal
+# fish_color_operator	cyan
+# fish_color_quote	718c00
+# fish_color_redirection	3e999f
+# fish_color_search_match	'--background=#3d3935'
+# fish_color_selection	red
+# fish_color_status	red
+# fish_color_user	'-o' 'green'
+# fish_color_valid_path	--underline
+# fish_color_normal, the default color
+# fish_color_command, the color for commands
+# fish_color_quote, the color for quoted blocks of text
+# fish_color_redirection, the color for IO redirections
+# fish_color_end, the color for process separators like ';' and '&'
+# fish_color_error, the color used to highlight potential errors
+# fish_color_param, the color for regular command parameters
+# fish_color_comment, the color used for code comments
+# fish_color_match, the color used to highlight matching parenthesis
+# fish_color_search_match, the color used to highlight history search matches
+# fish_color_operator, the color for parameter expansion operators like '*' and '~'
+# fish_color_escape, the color used to highlight character escapes like '\n' and '\x70'
+# fish_color_cwd, the color used for the current working directory in the default prompt
 
 set -x GTAGSLABEL pygments
 

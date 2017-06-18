bf3d0ef646a980f370af3710a72f0f522f952aa6
Aurélien Bottazzini <aurelien@bottazini.com>
2017-06-18 14:49:27 +0200

    sass setup and imenu

--- emacs/.emacs.d/lisp/init-css.el
+++ emacs/.emacs.d/lisp/init-css.el
@@ -1,31 +1,43 @@
+(defun imenu-setup ()
+  (progn
+    (setq imenu-case-fold-search nil)
+    (setq imenu-auto-rescan t)
+    (setq imenu-space-replacement " ")
+    (setq css-indent-offset 2)
+    (imenu-add-menubar-index)
+    ))
 (defun my-css-mode-setup ()
-  ;; todo adjust for sass
   (setq imenu-generic-expression
         '(("Selectors" "^[[:blank:]]*\\(.*[^ ]\\) *{" 1)))
-  (setq imenu-case-fold-search nil)
-  (setq imenu-auto-rescan t)
-  (setq imenu-space-replacement " ")
-  (setq css-indent-offset 2)
-  (imenu-add-menubar-index))
+  (imenu-setup))
+
 (add-hook 'css-mode-hook 'my-css-mode-setup)
 
 (use-package scss-mode
   :config
-  (add-hook 'scss-mode-hook 'my-css-mode-setup))
+  (add-hook 'scss-mode-hook '(lambda ()
+                               (setq imenu-generic-expression
+                                     '(("Selectors" "^[[:blank:]]*\\(.*[^ ]\\) *{" 1)))
+                               (imenu-setup))))
 
 (use-package sass-mode
   :mode "\\.sass\\'"
   :config
+  (add-hook 'sass-mode-hook
+            '(lambda ()
+               (setq imenu-generic-expression
+                     '(("Imports" "@import\\(.*[^ ]\\)" 1)))
+               (imenu-setup))
+            )
+
   (use-package highlight-indent-guides
     :config
     (setq highlight-indent-guides-method 'character))
   (add-hook 'sass-mode-hook 'my-css-mode-setup)
-  (add-hook 'sass-mode-hook 'highlight-indent-guides-mode)
-  )
+  (add-hook 'sass-mode-hook 'highlight-indent-guides-mode))
 
 (use-package less-css-mode
   :mode "\\.less\\'"
-;;  (add-hook 'less-css-mode-hook 'my-css-mode-setup)
   )
 
 (provide 'init-css)

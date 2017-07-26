e9a43b751da258f92c4775cd2a786758139af63d
Aurélien Bottazzini <aurelien@bottazini.com>
2017-07-25 20:36:04 -0700

    playing with find alternate file

--- emacs/.emacs.d/site-lisp/ab-find-alternate-file.el
+++ emacs/.emacs.d/site-lisp/ab-find-alternate-file.el
@@ -12,31 +12,39 @@
          (unit-test-dir (concat spec-dir "unit/"))
          (unit-test-specs-dir (concat unit-test-dir "specs/"))
          (e2e-test-dir (concat spec-dir "webdriver/specs/"))
+         (true-buffer-name (car (last (split-string (buffer-file-name) "/"))))
 
          (vue-filename (concat (car (split-string (buffer-name) ".spec.js")) ".vue"))
+         (js-filename (concat (car (split-string (buffer-name) ".spec.js")) ".js"))
          (spec-filename (concat (file-name-sans-extension (buffer-name)) ".spec.js"))
 
-         (file-relative-name-from-src-dir (file-relative-name (buffer-file-name) src-dir))
-         (file-relative-path (concat (car (split-string file-relative-name-from-src-dir (buffer-name)))))
+         (file-relative-name-from-src-dir (file-relative-name (buffer-name) src-dir))
+         (file-relative-path (concat (car (split-string file-relative-name-from-src-dir true-buffer-name))))
 
-         (spec-relative-name-from-spec-dir (file-relative-name (buffer-file-name) unit-test-specs-dir))
+         (spec-relative-name-from-spec-dir (file-relative-name (buffer-name) unit-test-specs-dir))
          (spec-relative-path (concat (car (split-string spec-relative-name-from-spec-dir (buffer-name)))))
 
-         (e2e-relative-name-from-spec-dir (file-relative-name (buffer-file-name) e2e-test-dir))
+         (e2e-relative-name-from-spec-dir (file-relative-name (buffer-name) e2e-test-dir))
          (e2e-relative-path (concat (car (split-string spec-relative-name-from-spec-dir (buffer-name)))))
          )
     (cond
-     ((string-suffix-p ".page.vue" (buffer-name))
+     ((string-suffix-p ".page.vue" true-buffer-name)
       (find-file (concat e2e-test-dir file-relative-path spec-filename)))
-     ((string-suffix-p ".page.spec.js" (buffer-name))
+     ((string-suffix-p ".page.spec.js" true-buffer-name)
       (find-file (concat src-dir e2e-relative-path vue-filename)))
-     ((string-suffix-p ".spec.js" (buffer-name))
-      (find-file (concat src-dir spec-relative-path vue-filename)))
-     ((string-suffix-p ".vue" (buffer-name))
+     ((string-suffix-p ".spec.js" true-buffer-name)
+      (cond
+       ((file-exists-p (concat src-dir spec-relative-path vue-filename)) (find-file (concat src-dir spec-relative-path vue-filename)))
+       ((file-exists-p (concat src-dir spec-relative-path js-filename)) (find-file (concat src-dir spec-relative-path js-filename)))
+       (t (find-file (concat src-dir spec-relative-path js-filename)))
+       ))
+     ((string-suffix-p ".vue" true-buffer-name)
       (find-file (concat unit-test-specs-dir file-relative-path spec-filename)))
-     ((string-suffix-p ".js" (buffer-name))
+     ((string-suffix-p ".js" true-buffer-name)
       (find-file (concat unit-test-specs-dir file-relative-path spec-filename)))
      (t (message "could not match file")))))
+
+(split-string "foo/bar/file.spec.js" "/")
 
 (provide 'ab-find-alternate-file)
 ;;; ab-find-alternate-file ends here

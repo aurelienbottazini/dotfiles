a1c474642927f488826fb2eeb6d1c9e1fa08bca1
Aurélien Bottazzini <aurelien@bottazini.com>
2017-09-24 19:04:54 +0200

    company-mode without company-tern

--- emacs/.emacs.d/lisp/init-completion.el
+++ emacs/.emacs.d/lisp/init-completion.el
@@ -1,2 +1,22 @@
+(use-package company
+  :diminish company-mode
+  :init
+  (add-hook 'after-init-hook 'global-company-mode)
+  :config
+
+  (let ((map company-active-map))
+    (define-key map (kbd "C-n") 'company-select-next)
+    (define-key map (kbd "C-p") 'company-select-previous)
+    (define-key map (kbd "C-f") 'company-complete-selection)
+    (define-key map (kbd "TAB") 'company-complete-selection)
+    (define-key map (kbd "<tab>") 'company-complete-selection)
+
+    )
+
+  (use-package company-tern
+    :diminish tern-mode
+    )
+  (setq company-backends '(company-files (company-capf company-dabbrev-code company-yasnippet)))
+  )
 
 (provide 'init-completion)

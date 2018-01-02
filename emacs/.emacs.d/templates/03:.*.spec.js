0b15058c6ed3ca45fc9ca00f0a8ad8f6fc6ee563
Aurélien Bottazzini <aurelien@bottazini.com>
2018-01-02 22:47:14 +0100

    update template

--- emacs/.emacs.d/templates/03:.*.spec.js
+++ emacs/.emacs.d/templates/03:.*.spec.js
@@ -1,33 +1,8 @@
-import { createVM, destroyVM } from '~test/unit/specs/utils';
-import ${1:`(file-name-nondirectory (car (split-string (buffer-file-name) ".spec.js")))`} from '~src/packages${2:`(cadr (split-string (file-name-directory (buffer-file-name)) "/packages"))`$1}.vue';
+import { shallow } from 'vue-test-utils';
+import ${1:`(file-name-nondirectory (car (split-string (buffer-file-name) ".spec.js")))`} from '../${2:`(cadr (split-string (file-name-directory (buffer-file-name)) "/packages"))`$1}.vue';
 
 describe('$1', () => {
-
-  it('is defined', () => {
-    expect($1).to.be.an('object');
-  });
-
-  describe('rendered component', () => {
-    let vm;
-
-    afterEach(() => {
-      destroyVM(vm);
-    });
-
-    function get$1() {
-      return vm.$el;
-    }
-
-    function create$1(template) {
-      return createVM({
-        template,
-        components: { $1 },
-      });
-    }
-
-    it('renders', () => {
-      vm = create$1('<$1></$1>');
-      expect(get$1()).to.be.defined;
-    });
+  it('renders', () => {
+    expect(shallow($1).isVueInstance()).to.be.true;
   });
 });

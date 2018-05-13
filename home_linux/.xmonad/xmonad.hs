e25c001a684d504b0fa541500d8177d4fd4d9046
Aurelien Bottazini <aurelien@bottazini.com>
2018-05-13 18:52:41 +0200

    xmonad screen setup

--- home_linux/.xmonad/xmonad.hs
+++ home_linux/.xmonad/xmonad.hs
@@ -2,7 +2,8 @@ import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run(spawnPipe)
-import XMonad.Util.EZConfig(additionalKeys)
+import XMonad.Util.EZConfig
+import qualified XMonad.StackSet as W
 import XMonad.Layout.Gaps
 import XMonad.Layout.Tabbed
 import XMonad.Layout.Accordion
@@ -20,6 +21,22 @@ mySpacing = 2
 
 mylayoutHook = maximize ((spacing mySpacing $ toggleLayouts (noBorders Full) (Tall 1 (3/100) (1/2)) ||| noBorders Full ||| noBorders (tabbed shrinkText def) |||  (spacing mySpacing $ Accordion)))
 myFocusFollowsMouse = False
+myKeys = [((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock; xset dpms force off")
+	, ((mod4Mask, xK_o), swapNextScreen)
+        , ((mod4Mask .|. controlMask, xK_Right), prevScreen)
+	, ((mod4Mask .|. controlMask, xK_Left),  nextScreen)
+	, ((mod4Mask .|. controlMask, xK_o),  shiftNextScreen)
+	, ((mod4Mask, xK_m), withFocused (sendMessage . maximizeRestore))
+	, ((mod4Mask, xK_u), spawn "amixer set Master 2+ unmute")
+	, ((mod4Mask, xK_d), spawn "amixer set Master 2- unmute")
+	, ((mod4Mask, xK_z), sendMessage ToggleLayout)
+        , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
+        , ((0, xK_Print), spawn "scrot")
+        ]
+        ++
+        [((m .|. mod1Mask, key), screenWorkspace sc >>= flip whenJust (windows . f)) -- Replace 'mod1Mask' with your mod key of choice.
+            | (key, sc) <- zip [xK_w, xK_e, xK_r] [1,0,2] -- was [0..] *** change to match your screen order ***
+            , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
 
 main = do
 
@@ -39,17 +56,4 @@ main = do
 				, ppCurrent = xmobarColor "#3a499c"  "" . wrap "[" "]"
 					, ppTitle = xmobarColor "#bc3e33" "" . shorten 50
                         }
-        } `additionalKeys`
-        [ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock; xset dpms force off")
-	, ((mod4Mask, xK_o), swapNextScreen)
-        , ((mod4Mask .|. controlMask, xK_Right), prevScreen)
-	, ((mod4Mask .|. controlMask, xK_Left),  nextScreen)
-	, ((mod4Mask .|. controlMask, xK_o),  shiftNextScreen)
-	, ((mod4Mask, xK_m), withFocused (sendMessage . maximizeRestore))
-	, ((mod4Mask, xK_u), spawn "amixer set Master 2+ unmute")
-	, ((mod4Mask, xK_d), spawn "amixer set Master 2- unmute")
-	, ((mod4Mask, xK_z), sendMessage ToggleLayout)
-        , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
-        , ((0, xK_Print), spawn "scrot")
-        ]
-
+        } `additionalKeys` myKeys
--- home_linux/.zprofile
+++ home_linux/.zprofile
@@ -1,3 +1,2 @@
-fpath+=('/home/aurelienbottazini/.nvm/versions/node/v8.11.1/lib/node_modules/pure-prompt/functions')
 autoload -U promptinit; promptinit
-prompt pure
+prompt suse

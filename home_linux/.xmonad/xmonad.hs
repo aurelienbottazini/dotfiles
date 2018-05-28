698125d6281d510311d8f4bf2ce8c1d673375bd9
Aurelien Bottazini <aurelien@bottazini.com>
2018-05-28 11:14:31 +0200

    xmonad background

--- home_linux/.xmonad/xmonad.hs
+++ home_linux/.xmonad/xmonad.hs
@@ -143,7 +143,7 @@ myProjects =
 myStartupHook = do
     setWMName "LG3D" -- workaround to make java swing windows work correctly. Without it they are just empty. For example Firefox -> file open.
     spawn "xsetroot -cursor_name left_ptr"
-    spawn "feh --bg-scale ~/Pictures/background.jpg"
+    spawn "feh --bg-scale ~/Dropbox/Pictures/background.jpg"
     -- spawn "xrandr --dpi 183 --fb 7860x4320 --output HDMI-3 --primary --mode 3840x2160 --pos 3840x0 --rotate normal --output HDMI-1 --off --output DP-1 --off --output HDMI-2 --mode 1920x1080 --scale 2x2 --pos 0x0 --rotate normal"
 
 noScratchPad ws = if ws == "NSP" then "" else ws

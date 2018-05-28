4e62dd7c19a145a4474bc03ea49e578c070a874b
Aurélien Bottazzini <aurelien@bottazini.com>
2018-05-28 19:06:27 +0200

    migration done

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -1,6 +1,6 @@
 ;; Load the rest of the packages
 (package-initialize t)
+(require 'cl)
 (require 'org)
 (require 'ob-tangle)
 (org-babel-load-file (expand-file-name "~/.emacs.d/Aurelien.org"))
-(put 'dired-find-alternate-file 'disabled nil)

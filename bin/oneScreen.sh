f7e06a024cbb9690d260a4d8bfcbb406d8c189fc
Aurelien Bottazini <aurelien@bottazini.com>
2018-05-30 14:20:59 +0200

    saving

--- bin/oneScreen.sh
+++ bin/oneScreen.sh
@@ -1,2 +1,2 @@
-xrandr --dpi 183 --output HDMI-3 --primary --mode 3840x2160 --pos 3840x0 --rotate normal --output HDMI-1 --off --output DP-1 --off --output HDMI-2 --off
+xrandr --dpi 183 --output HDMI-2 --primary --mode 3840x2160 --pos 3840x0 --rotate normal --output HDMI-1 --off --output DP-1 --off --output HDMI-3 --off
 xmonad --restart

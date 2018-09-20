3cadc1542bd5d5aaa5dbc7abed3c96dd700a2289
Aurélien Bottazini <aurelien@bottazini.com>
2018-09-20 10:22:32 +0200

    Clean modeline

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -57,7 +57,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#ffffff" :foreground "#000000" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight semi-light :height 111 :width normal :foundry "H&Co" :family "Operator Mono"))))
+ '(default ((t (:inherit nil :stipple nil :background "white" :foreground "#000000" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight semi-light :height 111 :width normal :foundry "H&Co" :family "Operator Mono"))))
  '(bold ((t (:weight normal))))
  '(bold-italic ((t (:slant italic :weight normal))))
  '(context-coloring-level-1-face ((t (:foreground "#4c4e56"))))

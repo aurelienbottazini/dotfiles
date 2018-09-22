29b0282672c550d05065b494d028f0e191ac5e62
Aurélien Bottazini <aurelien@bottazini.com>
2018-09-22 12:31:19 +0200

    rustup

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,6 +1,6 @@
 set fish_greeting
 
-set -x PATH ~/bin /usr/local/sbin ~/.rbenv/shims ~/.yarn/bin ~/projects/depot_tools $PATH
+set -x PATH ~/bin /usr/local/sbin ~/.rbenv/shims ~/.yarn/bin ~/.cargo/bin ~/projects/depot_tools $PATH
 
 set -x CDPATH $HOME $HOME/projects $HOME/work
 

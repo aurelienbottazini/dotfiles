68f63ba5b68233952cfd44ee12efccf19277921e
Aurélien Bottazini <aurelien@bottazini.com>
2018-09-22 11:08:14 +0200

    dox-do work with other repos and not only doximity

--- fish/.config/fish/functions/dox-do.fish
+++ fish/.config/fish/functions/dox-do.fish
@@ -1,3 +1,7 @@
 function dox-do
-  dox-dc exec doximity $argv
+  dox-dc exec (dox-repo) $argv
+end
+
+function dox-repo
+  git rev-parse --show-toplevel 2> /dev/null | xargs basename 2> /dev/null
 end

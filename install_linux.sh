477b8ea64aeb476548e3259a1934f742f70d0bf3
Aurélien Bottazini <aurelien@bottazini.com>
2018-10-02 10:27:35 +0200

    kitty

--- install_linux.sh
+++ install_linux.sh
@@ -161,3 +161,5 @@ sudo snap install skype
 
 # terminal
 curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
+ln -s ~/.local/kitty.app/bin/kitty ~/bin/
+sed -i "s/Icon\=kitty/Icon\=\/home\/$USER\/.local\/kitty.app\/share\/icons\/hicolor\/256x256\/apps\/kitty.png/g" ~/.local/share/applications/kitty.desktop

829935db0e084ee72e93803353bb409426acc61f
Aurélien Bottazini <aurelien@bottazini.com>
2018-10-03 11:50:34 +0200

    default font size

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -60,7 +60,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#f1f2f1" :foreground "#000000" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 1 :width normal :foundry "default" :family "default"))))
+ '(default ((t (:inherit nil :stipple nil :background "#f1f2f1" :foreground "#000000" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "default" :family "default"))))
  '(bold ((t (:weight normal))))
  '(bold-italic ((t (:slant italic :weight normal))))
  '(context-coloring-level-1-face ((t (:foreground "#4982cf"))))

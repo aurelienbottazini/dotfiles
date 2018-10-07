0cc74ae1b79fcdbcc8c259d759724445dd324462
Aurélien Bottazini <aurelien@bottazini.com>
2018-10-07 22:03:21 +0200

    trimage -> imageoptim

--- install_linux.sh
+++ install_linux.sh
@@ -168,3 +168,6 @@ ln -s ~/.local/kitty.app/bin/kitty ~/.local/bin/
 cp ~/.local/kitty.app/share/applications/kitty.desktop ~/.local/share/applications
 # Update the path to the kitty icon in the kitty.desktop file
 sed -i "s/Icon\=kitty/Icon\=\/home\/$USER\/.local\/kitty.app\/share\/icons\/hicolor\/256x256\/apps\/kitty.png/g" ~/.local/share/applications/kitty.desktop
+
+# imageoptim
+sudo apt-get install trimage

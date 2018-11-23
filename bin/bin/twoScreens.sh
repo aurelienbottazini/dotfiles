c2edcd3180eda0dff605158032bd59351ae97872
Aurélien Bottazini <aurelien@bottazini.com>
2018-11-23 09:34:59 +0100

    twoScreens workaround for second screen vertical

--- bin/bin/twoScreens.sh
+++ bin/bin/twoScreens.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
-xrandr --dpi 183 --output HDMI-2 --primary --mode 3840x2160 --pos 0x0 --rotate normal --output HDMI-1 --off --output DP-1 --off --output HDMI-3 --mode 1920x1080 --pos 3840x0 --rotate normal --panning 3840x2160+3840+0 --scale 2x2
+# two horizontal
+# xrandr --dpi 183 --output HDMI-2 --primary --mode 3840x2160 --pos 0x0 --rotate normal --output HDMI-1 --off --output DP-1 --off --output HDMI-3 --mode 1920x1080 --pos 3840x0 --rotate normal --panning 3840x2160+3840+0 --scale 2x2
 
-# vertical screen
+# one horizontal, vertical screen
 # xrandr --fb 6000x6000 --dpi 183 --output HDMI-2 --primary --mode 3840x2160 --pos 0x0 --rotate normal --output HDMI-1 --off --output DP-1 --off --output HDMI-3 --mode 1920x1080 --pos 3840x0 --scale 2x2 --rotate right --panning 2160x3840+3840+0
+
+# without panning so that second screen does not lose scaling
+xrandr --fb 6000x6000 --dpi 183 --output HDMI-2 --primary --mode 3840x2160 --pos 0x0 --rotate normal --output HDMI-1 --off --output DP-1 --off --output HDMI-3 --mode 1920x1080 --pos 3840x0 --scale 2x2 --rotate right
 
 # xmonad --restart

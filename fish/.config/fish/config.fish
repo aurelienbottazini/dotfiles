402c258de6203f79f3a34fbc86c950496c405772
Aurélien Bottazini <aurelien@bottazini.com>
2018-12-31 14:39:11 +0100

    ls colors with fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -61,7 +61,7 @@ set -x FZF_DEFAULT_OPTS "--color fg:-1,bg:-1,hl:33,fg+:254,bg+:235,hl+:33,info:1
 # 104	Light blue background
 # 105	Light purple background
 # 106	Turquoise background
-set -x LS_COLORS "rs=0:di=01;34:ln=00;92:mh=00:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=40;31;01:mi=00:su=37;41:sg=30;43:ca=30;41:tw=30;42:ow=34;42:st=37;44:ex=01;31:*.tar=
+set -x LS_COLORS "rs=0:di=00;34:ln=00;33:mh=00:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=40;31;01:mi=00:su=37;41:sg=30;43:ca=30;41:tw=30;42:ow=34;42:st=37;44:ex=01;31:*.tar=
 01;31:*.tgz=01;31:*.arc=01;31:*.arj=01;31:*.taz=01;31:*.lha=01;31:*.lz4=01;31:*.lzh=01;31:*.lzma=01;31:*.tlz=01;31:*.txz=01;31:*.tzo=01;31:*.t7z=01;31:*.zip=01;31:*.z=01;31:*.Z=0
 1;31:*.dz=01;31:*.gz=01;31:*.lrz=01;31:*.lz=01;31:*.lzo=01;31:*.xz=01;31:*.zst=01;31:*.tzst=01;31:*.bz2=01;31:*.bz=01;31:*.tbz=01;31:*.tbz2=01;31:*.tz=01;31:*.deb=01;31:*.rpm=01;
 31:*.jar=01;31:*.war=01;31:*.ear=01;31:*.sar=01;31:*.rar=01;31:*.alz=01;31:*.ace=01;31:*.zoo=01;31:*.cpio=01;31:*.7z=01;31:*.rz=01;31:*.cab=01;31:*.wim=01;31:*.swm=01;31:*.dwm=01

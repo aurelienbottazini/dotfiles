b155712ec4007331f34de0b33dbf881940fadad7
Aurélien Bottazini <aurelien@bottazini.com>
2019-03-22 16:47:51 +0100

    avy

--- emacs/.emacs.d/site-lisp/aurayb-narrow-indirect-vue.el
+++ emacs/.emacs.d/site-lisp/aurayb-narrow-indirect-vue.el
@@ -1,6 +1,10 @@
 ;;;  -*- lexical-binding: t; -*-
 (defun aurayb-make-narrow-indirect-vue (atag-string amode-fn)
   (lambda ()
+    "Creates an indirect buffer for a .vue file component and narrows it to the region
+ inside atag-string and set amode-fn as the mode file.
+If the indirect buffer already exists just switch to it.
+If already in the indirect buffer, switches bach to base-buffer."
     (interactive)
     (save-mark-and-excursion
       (let* ((indirect-vue-buffer-name (concat atag-string "->" (buffer-name)))
@@ -13,7 +17,7 @@
               (pop-to-buffer-same-window (make-indirect-buffer (buffer-name)  indirect-vue-buffer-name nil))
               (goto-char (point-max))
               (narrow-to-region (re-search-backward (concat "</" atag-string ">"))
-                                (progn (re-search-backward (concat "<" atag-string ">"))
+                                (progn (re-search-backward (concat "<" atag-string ".*>"))
                                        (next-line)
                                        (point)))
               (funcall amode-fn)

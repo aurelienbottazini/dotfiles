0f26bf7469c4dc4e34d3c105fe6e291aa5c9e747
Aurélien Bottazini <aurelien@bottazini.com>
2019-05-21 22:19:11 +0200

    font change

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -97,7 +97,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#f1f2f1" :foreground "#000000" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight light :height 111 :width normal :family "Operator Mono Book"))))
+ '(default ((t (:inherit nil :stipple nil :background "#f1f2f1" :foreground "#000000" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight light :height 130 :width normal :family "Operator Mono Book"))))
  '(bold ((t (:weight medium))))
  '(bold-italic ((t (:slant italic :weight medium))))
  '(italic ((t (:slant italic :weight medium))))

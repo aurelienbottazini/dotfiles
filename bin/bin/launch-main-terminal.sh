5eba1a6bf38390fe870c2011b298cd577dd3d27b
Aurélien Bottazini <aurelien@bottazini.com>
2019-05-24 07:49:20 +0200

    back to st with emoji font instructions

--- bin/bin/launch-main-terminal.sh
+++ bin/bin/launch-main-terminal.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-kitty --class main-terminal
+# note: st will crash if you have an emoji font with colors installed.
+# Use: ttf-symbola with has outlined emojis.
+st -g 200x80 -c main-terminal
+#kitty --class main-terminal

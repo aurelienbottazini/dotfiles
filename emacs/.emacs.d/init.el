c61b8644243d1317994ab6b9529cb61f4c590452
Aurélien Bottazini <aurelien.bottazini@gmail.com>
2019-10-01 12:19:59 +0200

    search.org

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -6,5 +6,6 @@
 (require 'ob-tangle)
 (setq vc-follow-symlinks t)
 (org-babel-load-file (expand-file-name "~/.emacs.d/Aurelien.org"))
+(org-babel-load-file (expand-file-name "~/.emacs.d/search.org"))
 (put 'magit-edit-line-commit 'disabled nil)
 (put 'narrow-to-region 'disabled nil)

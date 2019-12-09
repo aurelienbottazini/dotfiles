f6b83a89f68f73078632eff11d78400d2f4cfcc0
Aurélien Bottazini <aurelien.bottazini@gmail.com>
2019-12-09 23:17:53 +0100

    font

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -109,6 +109,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight light :height 130 :family "Operator Mono"))))
- '(bold ((t (:weight medium))))
- '(bold-italic ((t (:slant italic :weight medium)))))
+ '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight regular :height 130 :family "Input Mono"))))
+ '(bold ((t (:weight bold))))
+ '(bold-italic ((t (:slant italic :weight bold)))))

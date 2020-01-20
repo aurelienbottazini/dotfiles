b84c8743d752af63b23627b1359cb83c27590182
Aurélien Bottazini <aurelien.bottazini@gmail.com>
2020-01-20 19:50:25 +0100

    working on ctags

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -116,7 +116,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight regular :height 130 :family "Input Mono"))))
+ '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight regular :height 140 :family "Input Mono"))))
  '(bold ((t (:weight bold))))
  '(bold-italic ((t (:slant italic :weight bold))))
  '(context-coloring-level-0-face ((t (:foreground "#adaeb0"))))
